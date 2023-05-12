5a6c297dcae3615de50f0c6715fa1eb333416007
iqbalhossensorkar <iqbalhossen303@gmail.com>
2023-05-12 16:23:49 +0600

    third commit

--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -10,15 +10,31 @@ const Update = () => {
         const form = e.target;
         const name = form.name.value;
         const email = form.email.value;
-        console.log({name, email}); 
+        const updatedUser = { name, email }
+        console.log(updatedUser);
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(updatedUser)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount > 0) {
+                    alert("User Updated Successfully")
+                }
+            })
     }
     return (
         <div>
             <h3>Information about: {loadedUser.name}</h3>
             <form onSubmit={handleUpdate}>
-                <input type="text" name="name" id="" defaultValue={loadedUser.name}/>
+                <input type="text" name="name" id="" defaultValue={loadedUser.name} />
                 <br />
-                <input type="email" name="email" id="" defaultValue={loadedUser.email}/>
+                <input type="email" name="email" id="" defaultValue={loadedUser.email} />
                 <br />
                 <input type="submit" value="update" />
             </form>
